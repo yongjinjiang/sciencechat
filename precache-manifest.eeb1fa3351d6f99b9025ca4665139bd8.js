7d706a54e098c80417727891503b1ed72e3bea95
jyj <yjjiangphysics@gmail.com>
2019-11-18 13:38:22 -0600

    Updates

---
+++ precache-manifest.eeb1fa3351d6f99b9025ca4665139bd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa2292c147421749f1e6c69e908bf2ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2672a49dbe00153e5ac",
+    "url": "/static/css/main.680a205f.chunk.css"
+  },
+  {
+    "revision": "a216384aa0c2c4371cd5",
+    "url": "/static/js/2.c476cdce.chunk.js"
+  },
+  {
+    "revision": "c2672a49dbe00153e5ac",
+    "url": "/static/js/main.00a5b2d2.chunk.js"
+  },
+  {
+    "revision": "58a36b27d3ede6fc7033",
+    "url": "/static/js/runtime-main.a72496fc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
