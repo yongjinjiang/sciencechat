98598ad608b05a1fa5fcb44a90d85aa46a20323f
jyj <yjjiangphysics@gmail.com>
2019-11-20 11:05:16 -0600

    Updates

---
+++ precache-manifest.828592bd32289a8d92168228b41e73c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b8d4815bb1d7cc1d262daa46b8abb26",
+    "url": "/sciencechat/index.html"
+  },
+  {
+    "revision": "f63f471d1271cb546b5b",
+    "url": "/sciencechat/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "a5bdf9507f4f45d333db",
+    "url": "/sciencechat/static/css/main.5a856e0a.chunk.css"
+  },
+  {
+    "revision": "f63f471d1271cb546b5b",
+    "url": "/sciencechat/static/js/2.0daed6e4.chunk.js"
+  },
+  {
+    "revision": "a5bdf9507f4f45d333db",
+    "url": "/sciencechat/static/js/main.3642f09e.chunk.js"
+  },
+  {
+    "revision": "5f6a016abe40b69bee51",
+    "url": "/sciencechat/static/js/runtime-main.9dfbfa77.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/sciencechat/static/media/logo.25bf045c.svg"
+  }
+]);
